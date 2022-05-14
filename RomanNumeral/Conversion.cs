017c17e6ae823c3cf7485a74a48ec36def404648
vahidkianfar <vahidkianfar@gmail.com>
2022-05-15 00:02:22 +0100

    add dictionary

--- RomanNumeral/Conversion.cs
+++ RomanNumeral/Conversion.cs
@@ -8,6 +8,15 @@ public class Conversion
         UserInput = userInput.ToUpper();
     }
     public string UserInput { get; set; }
+    Dictionary<char, int> mappingCharacters = new()
+    { { 'I', 1 },
+        { 'V', 5 },
+        { 'X', 10 },
+        { 'L', 50 },
+        { 'C', 100 },
+        { 'D', 500 },
+        { 'M', 1000 }
+    };
     public bool RomanNumeralValidation(string UserInput)
     {
         const string regularExpression = "^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$";
